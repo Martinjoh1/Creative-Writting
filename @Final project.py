3dbea33bd3cf83e4c5c4b2e8e141a645220d0843
John Martin <Martinjoh@berea.edu>
2018-04-22 20:58:14 -0400

    nearly finished with the program, and I have made it so a person can choose one of the 4 font options.

--- @Final project.py
+++ @Final project.py
@@ -7,7 +7,7 @@
 # Purpose: Create beautiful word art to help in projects, or other activities
 ######################################################################
 import turtle
-
+import tkinter as TK
 class word:
 
     def __init__ (self,p=(0,0),):
@@ -19,14 +19,29 @@ class word:
 
     def letter_maker(self,wordmaker,letter):
         '''
-        :param self:
-        :param user_input_word:
-        :param user_input_color:
+        :param wordmaker:
+        :param letter:
         :return:
         '''
+
+
         user_input_color = input("Choose a color for your text:")
         wordmaker.color(user_input_color)
-        wordmaker.write(letter, move=False, align="left", font=("Arial", 75, "normal"))
+
+        fonts = input("Which font would you like to use? [vineta BT/Times New Roman/Castellar/Broadway]")
+        fonts_lists = ["vineta BT","Times New Roman","Castellar","Broadway"]
+
+        while fonts in fonts_lists:
+            if fonts == "vineta BT":
+                wordmaker.write(letter, move=False, align="left", font=("vineta BT", 75, "normal"))
+            elif fonts == "Times New Roman":
+                wordmaker.write(letter, move=False, align="left", font=("Times New Roman", 75, "normal"))
+            elif fonts == "Castellar":
+                wordmaker.write(letter, move=False, align="left", font=("Castellar", 75, "normal"))
+            elif fonts == "Broadway":
+                wordmaker.write(letter, move=False, align="left", font=("Broadway", 75, "normal"))
+
+
 
     def word_draw(self,wordmaker):
         '''
@@ -39,14 +54,21 @@ class word:
 
     def move (self,wordmaker):
          wordmaker.penup()
-         wordmaker.forward(20)
+         wordmaker.forward(40)
          wordmaker.pendown()
 
 
 
 
-def word_export():
-    pass
+def word_export(expwrd):
+    # tt = turtle.Turtle
+    # ts = turtle.Screen()
+    user_file_name = (input("What is your files name?"))+ ".eps"
+    readfile = open(user_file_name,"w")
+    readfile.write(expwrd)
+    # tt.getscreen(ts)
+    # ts.getcanvas().postscript(file= user_file_name)
+    readfile.close()
 
 def main():
     """
@@ -54,12 +76,18 @@ def main():
     :return: None
     """
     wn = turtle.Screen()
+    user_input_bcolor = input ("Choose a back ground color:")
+    wn.bgcolor(user_input_bcolor)
     wordmaker= turtle.Turtle()
-    wordmaker.penup()
-    wordmaker.backward(200)
-    wordmaker.pendown()
+    # wordmaker.hideturtle()
+    # wordmaker.penup()
+    # wordmaker.backward(200)
+    # wordmaker.pendown()
     word_information= word()
     word_information.word_draw(wordmaker)
+    word_export(word_information.user_input_word)
+    wn.getcanvas().postscript(file=word_information.user_input_word)
+    wn.exitonclick()
 
 
 
