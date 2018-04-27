c4b58246e1e141b67bca24ab308d5762ad704797
John Martin <Martinjoh@berea.edu>
2018-04-27 01:32:05 -0400

    Finally finished with something presentable.

--- @Final project test suite.py
+++ @Final project test suite.py
@@ -1,1 +1,3 @@
 import p01
+# there are no test suites becuse all of the information need for this program is user input, and there is no way to test user input.
+#Anything that is not user input is preset, and there is no need to test it, because it is not suppose to change.
